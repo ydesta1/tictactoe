f9500604b4997bea88fa953d1acc75f8b093f455
ydesta1 <yishak.a.desta@gmail.com>
2024-10-24 16:16:18 -0400

    added player 2 logic

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -6,10 +6,15 @@ public class tictactoe {
     public void tictactoe(){
     }
     public static void main(String[] args){
-        gameboard();
+        home_page();
 
     }
-    public static void gameboard(){
+    public static void home_page(){
+         //number of players
+        int num_of_players = get_num_of_players(); 
+        gameboard(num_of_players);
+    }
+    public static void gameboard(int num_of_players){
 
         ArrayList <String> positions = new ArrayList<String>();
         for (int i = 0; i < 10; i++){
@@ -20,15 +25,17 @@ public class tictactoe {
                 positions.get(0) + "|" + positions.get(1) + "|" + positions.get(2)+ "\n" + "---+---+---" + "\n" + positions.get(3) + "|" + positions.get(4) + "|" + positions.get(5)+ "\n" + "---+---+---" + "\n" + positions.get(6) + "|" + positions.get(7) + "|" + positions.get(8)
         );
         while (true){
-            if (play(positions)){
+            if (play(positions, num_of_players)){
                 break;
             };
         }
     }
-    public static boolean play(ArrayList<String> ALstr){
+    public static boolean play(ArrayList<String> ALstr, int num_of_players){
         boolean gameover = false;
         String playermark = " X ";
         String cpumark = " O ";
+        String player2mark = " O ";
+
 
         //player goes
         System.out.println("Enter your position (0-8)");
@@ -46,19 +53,38 @@ public class tictactoe {
             return gameover;
         };
 
-        //cpu goes
-        Random rand = new Random();
-        int cpupos = rand.nextInt(8);
-        while (ALstr.get(cpupos).equals(playermark)|| ALstr.get(cpupos).equals(cpumark)){
-            cpupos = rand.nextInt(8);
+        if (num_of_players == 1){
+             //cpu goes
+            Random rand = new Random();
+            int cpupos = rand.nextInt(8);
+            while (ALstr.get(cpupos).equals(playermark)|| ALstr.get(cpupos).equals(cpumark)){
+                cpupos = rand.nextInt(8);
+            }
+            ALstr.set(cpupos, " O ");
+            updateGameBoard(ALstr);
+            if (checkWinner(ALstr)){
+                gameover = true;
+                return gameover;
+            };
+        }else{
+            //player 2 goes
+            System.out.println("Player 2 enter your position (0-8)");
+            scan = new Scanner(System.in);
+            int player2pos = scan.nextInt();
+            while (ALstr.get(player2pos).equals(player2mark) || ALstr.get(player2pos).equals(playermark)){
+                System.out.println("Position is already taken!");
+                scan = new Scanner(System.in);
+                player2pos = scan.nextInt();
+            }
+            ALstr.set(player2pos, player2mark);
+            updateGameBoard(ALstr);
+            if (checkWinner(ALstr)){
+                gameover = true;
+                return gameover;
+            };
         }
-        ALstr.set(cpupos, " O ");
-
-        updateGameBoard(ALstr);
-        if (checkWinner(ALstr)){
-            gameover = true;
-            return gameover;
-        };
+       
+       
         return gameover;
     }
 
@@ -98,6 +124,13 @@ public class tictactoe {
 
                 positions.get(0) + "|" + positions.get(1) + "|" + positions.get(2)+ "\n" + "---+---+---" + "\n" + positions.get(3) + "|" + positions.get(4) + "|" + positions.get(5)+ "\n" + "---+---+---" + "\n" + positions.get(6) + "|" + positions.get(7) + "|" + positions.get(8)
         );
+    }
+
+    public static int get_num_of_players(){
+        System.out.println("how many players (1 or 2)?");
+        Scanner scan = new Scanner (System.in);
+        int numOfPlayers = scan.nextInt();
+        return numOfPlayers; 
     }
 
     public static void ClearConsole(){
